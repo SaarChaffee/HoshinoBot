839e7d04f1a881969d3cb7cd9789ba6760b8757c
SaarChaffee <1240148332@qq.com>
2020-06-16 19:10:19 +0800

    自定义

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -11,66 +11,57 @@ from hoshino.service import Service, Privilege as Priv
 
 @on_command('zai?', aliases=('在?', '在？', '在吗', '在么？', '在嘛', '在嘛？'))
 async def say_hello(session):
-    await session.send('はい！私はいつも貴方の側にいますよ！')
+    await session.send('あなたのために言っておくけど、私からは少なくとも2メートル以上離れることね......')
 
 sv = Service('chat', manage_priv=Priv.SUPERUSER, visible=False)
 
 
 @sv.on_command('沙雕机器人', aliases=('沙雕機器人',), only_to_me=False)
 async def say_sorry(session):
-    await session.send('ごめんなさい！嘤嘤嘤(〒︿〒)')
+    await session.send('ごめんなさい！')
 
 
 @sv.on_command('老婆', aliases=('waifu', 'laopo'), only_to_me=True)
 async def chat_waifu(session):
-    if not sv.check_priv(session.ctx, Priv.SUPERUSER):
-        await session.send(R.img('laopo.jpg').cqcode)
-    else:
-        await session.send('mua~')
+    await session.send('まったく、あなたという人は本当に頑固ね……これじゃ私、もう全力であなたを守るしかないじゃない。')
 
 
 @sv.on_command('老公', only_to_me=True)
 async def chat_laogong(session):
-    await session.send('你给我滚！', at_sender=True)
+    await session.send('またたきせずに自分の死に様を見届けることね!', at_sender=True)
 
 
 @sv.on_command('mua', only_to_me=True)
 async def chat_mua(session):
-    await session.send('笨蛋~', at_sender=True)
-
-
-@sv.on_command('来点星奏', only_to_me=False)
-async def seina(session):
-    await session.send(R.img('星奏.png').cqcode)
+    await session.send('バカ', at_sender=True)
 
 
 @sv.on_command('我有个朋友说他好了', aliases=('我朋友说他好了', ), only_to_me=False)
 async def ddhaole(session):
-    await session.send('那个朋友是不是你弟弟？')
-    await util.silence(session.ctx, 0)
+    await session.send('周りに誰もいない部屋をお願い...')
 
 
 @sv.on_command('我好了', only_to_me=False)
 async def nihaole(session):
-    # await session.send('不许好，憋回去！')
-    await util.silence(session.ctx, 0)
+    await session.send('ちょっと近いわ……')
+    # await util.silence(session.ctx, 0)
 
 # ============================================ #
 
 
-@sv.on_keyword(('确实', '有一说一', 'u1s1', 'yysy'))
-async def chat_queshi(bot, ctx):
-    if random.random() < 0.05:
-        await bot.send(ctx, R.img('确实.jpg').cqcode)
+# @sv.on_keyword(('确实', '有一说一', 'u1s1', 'yysy'))
+# async def chat_queshi(bot, ctx):
+#     if random.random() < 0.05:
+#         await bot.send(ctx, R.img('确实.jpg').cqcode)
 
 
-@sv.on_keyword(('会战', '刀'))
-async def chat_clanba(bot, ctx):
-    if random.random() < 0.03:
-        await bot.send(ctx, R.img('我的天啊你看看都几度了.jpg').cqcode)
+# @sv.on_keyword(('会战', '刀'))
+# async def chat_clanba(bot, ctx):
+#     if random.random() < 0.03:
+#         await bot.send(ctx, R.img('我的天啊你看看都几度了.jpg').cqcode)
 
 
-@sv.on_keyword(('内鬼'))
-async def chat_neigui(bot, ctx):
-    if random.random() < 0.10:
-        await bot.send(ctx, R.img('内鬼.png').cqcode)
+# @sv.on_keyword(('内鬼'))
+# async def chat_neigui(bot, ctx):
+#     if random.random() < 0.10:
+#         await bot.send(ctx, R.img('内鬼.png').cqcode)
