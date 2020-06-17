545226ee26f81a1fb2d0f4983ff1957e4cd6aac5
SaarChaffee <1240148332@qq.com>
2020-06-17 17:36:42 +0800

    自定义

--- hoshino/modules/groupmaster/anti_abuse.py
+++ hoshino/modules/groupmaster/anti_abuse.py
@@ -15,8 +15,10 @@ from hoshino import logger, util, Service, R
 bot = nonebot.get_bot()
 BLANK_MESSAGE = Message(MessageSegment.text(''))
 
+
 @message_preprocessor
-async def black_filter(bot, ctx, plugin_manager=None):  # plugin_manager is new feature of nonebot v1.6
+# plugin_manager is new feature of nonebot v1.6
+async def black_filter(bot, ctx, plugin_manager=None):
     first_msg_seg = ctx['message'][0]
     if first_msg_seg.type == 'hb':
         return  # pass normal Luck Money Pack to avoid abuse
@@ -46,7 +48,8 @@ async def hb_handler(ctx):
             Service.set_block_user(user_id, timedelta(days=30))
             util.silence(ctx, 7 * 24 * 60 * 60)
             msg_from = f"{ctx['user_id']}@[群:{ctx['group_id']}]"
-            logger.critical(f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from} detected as abuse: {ctx["message"]}')
+            logger.critical(
+                f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from} detected as abuse: {ctx["message"]}')
             await bot.send(ctx, "检测到滥用行为，您的操作已被记录并加入黑名单。\nbot拒绝响应本群消息1小时", at_sender=True)
             try:
                 await bot.set_group_kick(self_id=self_id, group_id=group_id, user_id=user_id, reject_add_request=True)
@@ -58,10 +61,12 @@ async def hb_handler(ctx):
 # ============================================ #
 
 BANNED_WORD = (
-    'rbq', 'RBQ', '憨批', '废物', '死妈', '崽种', '傻逼', '傻逼玩意', 
-    '没用东西', '傻B', '傻b', 'SB', 'sb', '煞笔', 'cnm', '爬', 'kkp', 
+    'rbq', 'RBQ', '憨批', '废物', '死妈', '崽种', '傻逼', '傻逼玩意',
+    '没用东西', '傻B', '傻b', 'SB', 'sb', '煞笔', 'cnm', '爬', 'kkp',
     'nmsl', 'D区', '口区', '我是你爹', 'nmbiss', '弱智', '给爷爬', '杂种爬'
 )
+
+
 @on_command('ban_word', aliases=BANNED_WORD, only_to_me=True)
 async def ban_word(session):
     ctx = session.ctx
@@ -71,9 +76,10 @@ async def ban_word(session):
         msg_from += f'@[群:{ctx["group_id"]}]'
     elif ctx['message_type'] == 'discuss':
         msg_from += f'@[讨论组:{ctx["discuss_id"]}]'
-    logger.critical(f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from}: {ctx["message"]}')
+    logger.critical(
+        f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from}: {ctx["message"]}')
     # await session.send(random.choice(BANNED_WORD))
     Service.set_block_user(user_id, timedelta(hours=8))
     pic = R.img(f"chieri{random.randint(1, 4)}.jpg").cqcode
-    await session.send(f"不理你啦！バーカー\n{pic}", at_sender=True)
-    await util.silence(session.ctx, 8*60*60)
+    await session.send(f"バーカー", at_sender=True)
+    # await util.silence(session.ctx, 8*60*60)
