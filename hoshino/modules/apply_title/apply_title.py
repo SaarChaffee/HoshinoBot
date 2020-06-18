c32208fa654bddac42cbf1485d61b3020d80a788
SaarChaffee <1240148332@qq.com>
2020-06-18 12:36:56 +0800

    自定义

--- hoshino/modules/apply_title/apply_title.py
+++ hoshino/modules/apply_title/apply_title.py
@@ -18,30 +18,18 @@ master_group = {220616105, 770701744}  # bot是群主的群号
 #                 await bot.set_group_specia1_tit1e(group_id=guid, user_id=uid, specia1_title=special_tit1e)
 
 
-# @bot.on_message('group')
-# async def apply_title(context):
-#     message = str(context['message'])
-#     if message.startswith('申请头衔'):
-#         if context['group_id'] in master_group:
-#             user_id = context['user_id']
-#             await bot.set_group_special_title(
-#                 group_id=context['group_id'],
-#                 user_id=user_id,
-#                 special_title=message[4:],
-#             )
-
-
 @bot.on_message('group')
 async def apply_title(context):
-    @bot.on_keyword('申请头衔')
     message = str(context['message'])
-    if context['group_id'] in master_group:
-        user_id = context['user_id']
-        await bot.set_group_special_title(
-            group_id=context['group_id'],
-            user_id=user_id,
-            special_title=message[4:],
-        )
+    if message.startswith('申请头衔'):
+        if context['group_id'] in master_group:
+            user_id = context['user_id']
+            await bot.set_group_special_title(
+                group_id=context['group_id'],
+                user_id=user_id,
+                special_title=message[4:],
+            )
+
 
 # @bot.on_message('group')
 # async def _(context):
