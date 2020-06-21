34ff372f8fd8cb058c9aeceece3b5533e273f423
SaarChaffee <1240148332@qq.com>
2020-06-21 10:49:03 +0800

    自定义

--- hoshino/modules/apply_title/apply_title.py
+++ hoshino/modules/apply_title/apply_title.py
@@ -7,17 +7,6 @@ last_req = {}
 master_group = {220616105, 770701744}  # bot是群主的群号
 
 
-# @bot.on_message('group')
-# async def apply_title(context):
-#     if event.group_id in master_group:
-#         message = str(event.message)
-#         if message.startswith("申请头衔"):
-#             special_title = message[4:]
-#             try:
-#                 uid = event.user_id
-#                 await bot.set_group_specia1_tit1e(group_id=guid, user_id=uid, specia1_title=special_tit1e)
-
-
 @bot.on_message('group')
 async def apply_title(context):
     message = str(context['message'])
@@ -29,17 +18,3 @@ async def apply_title(context):
                 user_id=user_id,
                 special_title=message[4:],
             )
-
-
-# @bot.on_message('group')
-# async def _(context):
-#     if context['message'].startswith('申请头衔'):
-#         if context['group_id'] in master_group:
-#             user_id = context['user_id']
-#             if(1):
-#                 return {'reply': '新头衔要好好佩戴哦', 'at_sender': False}
-#             await bot.set_group_special_title(
-#                 group_id=context['group_id'],
-#                 user_id=user_id,
-#                 special_title=message[4:],
-#             )
