7e863c3d05ecb8b6bec17789ffc0be52cfc3ab64
霞飛 <saarchaffee@qq.com>
2025-05-23 13:25:52 +0800

    chore: remove frequency limit

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -1,10 +1,9 @@
 from hoshino.typing import CQEvent
-from hoshino.util import FreqLimiter, filt_message
+from hoshino.util import filt_message
 
 from .. import chara
 from . import sv
 
-lmt = FreqLimiter(5)
 
 @sv.on_suffix('是谁')
 @sv.on_prefix('谁是')
@@ -23,11 +22,6 @@ async def whois(bot, ev: CQEvent):
     if confi < 60:
         return
 
-    uid = ev.user_id
-    if not lmt.check(uid):
-        await bot.finish(ev, f'兰德索尔花名册冷却中(剩余 {int(lmt.left_time(uid)) + 1}秒)', at_sender=True)
-
-    lmt.start_cd(uid, 120 if guess else 0)
     if guess:
         name = filt_message(name)
         msg = f'兰德索尔似乎没有叫"{name}"的人...\n角色别称补全计划: github.com/Ice9Coffee/LandosolRoster'
